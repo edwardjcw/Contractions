250ba120354331a64b07b1e8046746e4c9480f65
Edward Conrad <edwardjcw@gmail.com>
2013-07-03 10:41:19 -0500

    Initial Commit

---
+++ Contractions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Contractions
+//
+//  Created by Edward Conrad on 7/3/13.
+//  Copyright (c) 2013 Edward Conrad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
